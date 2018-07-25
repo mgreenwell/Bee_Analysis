c31e5558f0fdd1f7ef7b5aea97e5ec4f3e9563fb
mgreenwell <matthew.greenwell@pgr.reading.ac.uk>
2018-07-25 16:09:25 +0100

    ADD code for if theta is greater than 0

--- Scripts/Removing species additionally for bean pollinators method 2.R
+++ Scripts/Removing species additionally for bean pollinators method 2.R
@@ -31,6 +31,10 @@ data <- species_data %>%
 
 # ========================== Writing loops ====================================
 
+
+# ================== Script if theta is less than zero ========================
+
+
 # Create list of species from growth.rate data
 
 bean_species_list <- unique(data$species)
@@ -57,11 +61,126 @@ for(i in bean_species_list){  # Open loop
     group_by(year) %>%
     summarise(mean.growth.rate = mean(growth.rate.dif, na.rm = TRUE))
   
+  bean_growth_rate_mean1 <- bean_growth_rate_mean
   
   # Set all values above theta (-0.1 in this example) to equal theta
   
-  bean_growth_rate_mean$mean.growth.rate[bean_growth_rate_mean$mean.growth.rate > -0.1] <- -0.1
+  bean_growth_rate_mean1$mean.growth.rate[bean_growth_rate_mean1$mean.growth.rate > -0.1] <- -0.1
   
+  
+  # Create plot of mean yearly growth rates across all species using subset of data
+  
+  bean_mean_growth_rate_plot <- ggplot(
+    bean_growth_rate_mean1, aes(
+      x = year,
+      y = mean.growth.rate)) + 
+    geom_hline(yintercept = 0) +
+    coord_cartesian(xlim = c(1985, 2015), ylim = c(-1.5, 2)) +
+    scale_x_continuous(breaks = seq(1985, 2015, by = 2)) +
+    scale_y_continuous(breaks=seq(-1.5, 2, 0.1)) +
+    geom_line() +
+    theme_classic() +
+    ggtitle(i, "Removed") 
+  
+  
+  # Plot graph
+  
+  plot(bean_mean_growth_rate_plot)
+  
+  
+  # Create a new df of the order of years in the data
+  
+  id <- order(bean_growth_rate_mean1$year)
+  
+  # Cacluate area under the curve i.e. area between line and zero
+  
+  AUC <-
+    sum(
+      diff(bean_growth_rate_mean1$year[id]) * 
+        rollmean(bean_growth_rate_mean1$mean.growth.rate[id], 2)
+    )
+  
+  
+  # create new df where all points have the value of theta
+  
+  y2 <- rep(-0.1, 31)
+  df<- cbind(bean_growth_rate_mean1$year,y2)
+  df <- as.data.frame(df)
+  
+  # Plot
+  
+  # ggplot(df, aes(growth.rate.mean$year, y2)) +geom_line() 
+  
+  
+  # Calculate area between theta and zero
+  
+  AUC2 <- sum(diff(bean_growth_rate_mean1$year[id])*rollmean(y2[id],2))
+  
+  
+  # Subtract area between theta and zero from original calculation of AUC to get only area
+  # between the line and theta.
+  
+  deficit_value <- AUC-AUC2
+  
+  
+  # Set as datafram
+  
+  deficit_value <- as.data.frame(cbind(deficit_value, i))
+  
+  
+  # Bind to deficit dataframe. Each loop adds new rows with a different species removed
+  
+  deficit  <- rbind(deficit_value, deficit)
+  
+  
+} # Close loop
+
+
+# See calculated deficits
+deficit
+
+
+# Only works if theta less than zero 
+# need to work out how to calc if theta greater than zero.
+
+
+# ================== Script if theta is greater than zero =====================
+
+
+# Create list of species from growth.rate data
+
+bean_species_list <- unique(data$species)
+
+bean_growth_rate_mean2 <- bean_growth_rate_mean
+
+deficit <- NULL
+# Write for loop
+# loop creates a subset of data removing one species each time (species i)
+# Plots yearly mean growth rates minus species i each time
+# Creates n plots where n is number of species in list
+
+for(i in bean_species_list){  # Open loop
+  
+  
+  # a gets a subset of growth.rate data minus species i.
+  
+  bean_removed_species <- subset(data, species != i) 
+  
+  
+  # Growth.rate.mean gets removed_species 
+  # removed_species is grouped by year
+  # yearly mean growthrates of all species are calulated
+  
+  bean_growth_rate_mean <- bean_removed_species %>% 
+    group_by(year) %>%
+    summarise(mean.growth.rate = mean(growth.rate.dif, na.rm = TRUE))
+  
+  bean_growth_rate_mean2 <- bean_growth_rate_mean
+  
+  # Set all values above theta (-0.1 in this example) to equal theta
+  
+  bean_growth_rate_mean$mean.growth.rate[bean_growth_rate_mean$mean.growth.rate > 0.1] <- 0.1
+  bean_growth_rate_mean$mean.growth.rate[bean_growth_rate_mean$mean.growth.rate < 0] <- 0
   
   # Create plot of mean yearly growth rates across all species using subset of data
   
@@ -96,9 +215,53 @@ for(i in bean_species_list){  # Open loop
     )
   
   
+  
+  
+  
+  # Set all values above theta (-0.1 in this example) to equal theta
+  
+  bean_growth_rate_mean2$mean.growth.rate[bean_growth_rate_mean2$mean.growth.rate > 0] <- 0
+  
+  
+  # Create plot of mean yearly growth rates across all species using subset of data
+  
+  bean_mean_growth_rate_plot <- ggplot(
+    bean_growth_rate_mean2, aes(
+      x = year,
+      y = mean.growth.rate)) + 
+    geom_hline(yintercept = 0) +
+    coord_cartesian(xlim = c(1985, 2015), ylim = c(-1.5, 2)) +
+    scale_x_continuous(breaks = seq(1985, 2015, by = 2)) +
+    scale_y_continuous(breaks=seq(-1.5, 2, 0.1)) +
+    geom_line() +
+    theme_classic() +
+    ggtitle(i, "Removed") 
+  
+  
+  # Plot graph
+  
+  plot(bean_mean_growth_rate_plot)
+  
+  
+  # Create a new df of the order of years in the data
+  
+  id <- order(bean_growth_rate_mean2$year)
+  
+  # Cacluate area under the curve i.e. area between line and zero
+  
+  AUC2 <-
+    sum(
+      diff(bean_growth_rate_mean2$year[id]) * 
+        rollmean(bean_growth_rate_mean2$mean.growth.rate[id], 2)
+    )
+  
+  
+  
+  
+  
   # create new df where all points have the value of theta
   
-  y2 <- rep(-0.1, 31)
+  y2 <- rep(0.1, 31)
   df<- cbind(bean_growth_rate_mean$year,y2)
   df <- as.data.frame(df)
   
@@ -109,13 +272,13 @@ for(i in bean_species_list){  # Open loop
   
   # Calculate area between theta and zero
   
-  AUC2 <- sum(diff(bean_growth_rate_mean$year[id])*rollmean(y2[id],2))
+  AUC3 <- sum(diff(bean_growth_rate_mean$year[id])*rollmean(y2[id],2))
   
   
   # Subtract area between theta and zero from original calculation of AUC to get only area
   # between the line and theta.
   
-  deficit_value <- AUC-AUC2
+  deficit_value <- AUC2 - AUC3 + AUC
   
   
   # Set as datafram
@@ -133,9 +296,3 @@ for(i in bean_species_list){  # Open loop
 
 # See calculated deficits
 deficit
-
-
-# Only works if theta less than zero 
-# need to work out how to calc if theta greater than zero.
-
-
